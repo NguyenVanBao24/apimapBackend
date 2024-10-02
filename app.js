8faf3bd810d7f0ece677b1193082cf1fced26e0a
Bảo <nguyenvanbaom1515@gmail.com>
2024-10-02 08:59:30 +0700

    first commit 10

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors'); // Thêm dòng này
 const userRoutes = require('./routes/userRoutes');
 
 dotenv.config();
 
 const app = express();
-app.use(express.json()); // Để phân tích dữ liệu JSON từ body request
+app.use(cors()); // Sử dụng cors để cho phép tất cả các origin
+app.use(express.json());
 
 // Sử dụng router
 app.use('/api/users', userRoutes);
 
 // Chạy server
-const PORT = process.env.PORT || 8081;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
